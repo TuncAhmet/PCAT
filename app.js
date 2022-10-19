5c1c88a59d2db6e6c882d3d0c1e2fc5bc2ee3c5f
TuncAhmet <tunch.ahmet@gmail.com>
2022-10-19 03:19:24 +0300

    commit for patika hw-4

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//VİEW ENGİNE
+app.set('view engine', 'ejs');
+
+//MİDDLEWARE
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server ${port} portunda dinleniyor`);
+});
